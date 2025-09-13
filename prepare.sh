b4a379010969b4f8b37d46df213bb069bceb4915
celeroon <37227951+celeroon@users.noreply.github.com>
2025-09-13 10:51:24 +0200

    Update with OpenSSL system default

--- prepare.sh
+++ prepare.sh
@@ -20,6 +20,8 @@ mkdir ./record >/dev/null 2>&1
 chmod -R 777 ./record
 echo "done"
 echo "Creating SSL certificates"
+# Set OpenSSL config to use system default
+export OPENSSL_CONF=/etc/ssl/openssl.cnf
 openssl req -nodes -newkey rsa:2048 -new -x509 -keyout nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
 echo "You can use your own certificates by placing the private key in nginx/ssl/self-ssl.key and the cert in nginx/ssl/self.cert"
 echo "done"
